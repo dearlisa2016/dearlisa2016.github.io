57370710a0fd87ce1a6f8c1dba1fed6d624264b8
dearlisa2016 <dearlisa2016@gmail.com>
2016-08-10 11:33:52 +0800

    personal website
    1st uploaded

---
+++ Popular Movie Gallery/photo/data.js
@@ -0,0 +1,164 @@
+var data = [];
+var dataStr = '1、Suicide Squad<br>\
+<br>\
+·Director: David Ayer<br>\
+·Writer: David Ayer<br>\
+·Stars: Will Smith, Jared Leto, Margot Robbie<br>\
+·A secret government agency recruits a group of imprisoned supervillains to execute dangerous black ops missions in exchange for clemency, which inevitably leads to chaos. <br>\
+<br>\
+<br>\
+2、Stranger Things<br>\
+<br>\
+·Creators: Matt Duffer, Ross Duffer<br>\
+·Stars: Winona Ryder, David Harbour, Finn Wolfhard<br>\
+·When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back. <br>\
+<br>\
+<br>\
+3、Jason Bourne<br>\
+<br>\
+·Director: Paul Greengrass<br>\
+·Writers: Paul Greengrass, Christopher Rouse<br>\
+·Stars: Matt Damon, Tommy Lee Jones, Alicia Vikander<br>\
+·The CIA\'s most dangerous former operative is drawn out of hiding to uncover more explosive truths about his past. <br>\
+<br>\
+<br>\
+4、Star Trek Beyond<br>\
+<br>\
+·Director: Justin Lin<br>\
+·Writers: Simon Pegg, Doug Jung<br>\
+·Stars: Chris Pine, Zachary Quinto, Karl Urban<br>\
+·The USS Enterprise crew explores the furthest reaches of uncharted space, where they encounter a new ruthless enemy who puts them and everything the Federation stands for to the test. <br>\
+<br>\
+<br>\
+5、Game of Thrones<br>\
+<br>\
+·Creators: David Benioff, D.B. Weiss<br>\
+·Stars: Emilia Clarke, Peter Dinklage, Kit Harington<br>\
+·While a civil war brews between several noble families in Westeros, the children of the former rulers of the land attempt to rise up to power. Meanwhile a forgotten race, bent on destruction, plans to return after thousands of years in the North. <br>\
+<br>\
+<br>\
+6、The Night Of<br>\
+<br>\
+·Creators: Richard Price, Steven Zaillian<br>\
+·Stars: Riz Ahmed, Peyman Moaadi, Poorna Jagannathan<br>\
+·After a night of partying with a female stranger, a man wakes up to find her stabbed to death and is charged with her murder. <br>\
+<br>\
+<br>\
+7、Mr. Robot<br>\
+<br>\
+·Creator: Sam Esmail<br>\
+·Stars: Rami Malek, Christian Slater, Portia Doubleday<br>\
+·Mr. Robot follows Elliot, a young programmer who works as a cyber-security engineer by day and a vigilante hacker by night. Elliot finds himself at a crossroad when the mysterious leader of an underground hacker group recruits him to destroy the corporation he is paid to protect. <br>\
+<br>\
+<br>\
+8、Preacher<br>\
+<br>\
+·Creators: Sam Catlin, Evan Goldberg, Seth Rogen<br>\
+·Stars: Dominic Cooper, Joseph Gilgun, Ruth Negga<br>\
+·After a supernatural event at his church, a preacher enlists the help of a vampire to find God.<br>\
+<br>\
+<br>\
+9、Nerve<br>\
+<br>\
+·Directors: Henry Joost, Ariel Schulman<br>\
+·Writers: Jeanne Ryan (novel), Jessica Sharzer<br>\
+·Stars: Emma Roberts, Dave Franco, Emily Meade<br>\
+A high school senior finds herself immersed in an online game of truth or dare, where her every move starts to become manipulated by an anonymous community of "watchers." <br>\
+<br>\
+<br>\
+10、Orange Is the New Black<br>\
+<br>\
+·Creator: Jenji Kohan<br>\
+·Stars: Taylor Schilling, Danielle Brooks, Taryn Manning<br>\
+·The story of Piper Chapman, a woman in her thirties who is sentenced to fifteen months in prison after being convicted of a decade-old crime of transporting money to her drug-dealing girlfriend.  <br>\
+<br>\
+<br>\
+11、Bad Moms<br>\
+<br>\
+·Directors: Jon Lucas, Scott Moore<br>\
+·Writers: Jon Lucas, Scott Moore<br>\
+·Stars: Mila Kunis, Kathryn Hahn, Kristen Bell<br>\
+·When three overworked and under-appreciated moms are pushed beyond their limits, they ditch their conventional responsibilities for a jolt of long overdue freedom, fun, and comedic self-indulgence.<br>\
+<br>\
+<br>\
+12、The Secret Life of Pets<br>\
+<br>\
+·Directors: Yarrow Cheney, Chris Renaud<br>\
+·Writers: Cinco Paul, Ken Daurio<br>\
+·Stars: Louis C.K., Eric Stonestreet, Kevin Hart<br>\
+·The quiet life of a terrier named Max is upended when his owner takes in Duke, a stray whom Max instantly dislikes. <br>\
+<br>\
+<br>\
+13、Ghostbusters<br>\
+<br>\
+·Director: Paul Feig<br>\
+·Writers: Katie Dippold, Paul Feig<br>\
+·Stars: Melissa McCarthy, Kristen Wiig, Kate McKinnon<br>\
+·Following a ghost invasion of Manhattan, paranormal enthusiasts Erin Gilbert and Abby Yates, nuclear engineer Jillian Holtzmann, and subway worker Patty Tolan band together to stop the otherworldly threat. <br>\
+<br>\
+<br>\
+14、Tallulah<br>\
+<br>\
+·Director: Sian Heder<br>\
+·Writer: Sian Heder<br>\
+·Stars: Ellen Page, Allison Janney, Tammy Blanchard<br>\
+·Desperate to be rid of her toddler, a dissatisfied Beverly Hills housewife hires a stranger to babysit and ends up getting much more than she bargained for. <br>\
+<br>\
+<br>\
+15、Finding Dory<br>\
+<br>\
+·Directors: Andrew Stanton, Angus MacLane<br>\
+·Writers: Andrew Stanton (original story by), Andrew Stanton (screenplay)<br>\
+·Stars: Ellen DeGeneres, Albert Brooks, Ed O\'Neill<br>\
+·The friendly but forgetful blue tang fish begins a search for her long-lost parents, and everyone learns a few things about the real meaning of family along the way. <br>\
+<br>\
+<br>\
+16、Zootopia<br>\
+<br>\
+·Directors: Byron Howard, Rich Moore<br>\
+·Writers: Byron Howard (story by), Rich Moore (story by)<br>\
+·Stars: Ginnifer Goodwin, Jason Bateman, Idris Elba<br>\
+·In a city of anthropomorphic animals, a rookie bunny cop and a cynical con artist fox must work together to uncover a conspiracy. <br>\
+<br>\
+<br>\
+17、The Nice Guys<br>\
+<br>\
+·Director: Shane Black<br>\
+·Writers: Shane Black, Anthony Bagarozzi<br>\
+·Stars: Russell Crowe, Ryan Gosling, Angourie Rice<br>\
+·A mismatched pair of private eyes investigate the apparent suicide of a porn star in 1970s Los Angeles.<br>\
+<br>\
+<br>\
+18、Me Before You<br>\
+<br>\
+·Director: Thea Sharrock<br>\
+·Writers: Jojo Moyes (screenplay), Jojo Moyes (novel)<br>\
+·Stars: Emilia Clarke, Sam Claflin, Janet McTeer<br>\
+·A girl in a small town forms an unlikely bond with a recently-paralyzed man she\'s taking care of. <br>\
+<br>\
+<br>\
+19、Blood Father<br>\
+<br>\
+·Director: Jean-François Richet<br>\
+·Writers: Peter Craig (screenplay), Andrea Berloff (screenplay)<br>\
+·Stars: Mel Gibson, Erin Moriarty, Diego Luna<br>\
+·An ex-con reunites with his estranged wayward 16-year old daughter to protect her from drug dealers who are trying to kill her.<br>\
+<br>\
+<br>\
+20、Captain Fantastic<br>\
+<br>\
+·Director: Matt Ross<br>\
+·Writer: Matt Ross<br>\
+·Stars: Viggo Mortensen, George MacKay, Samantha Isler<br>\
+·In the forests of the Pacific Northwest, a father devoted to raising his six kids with a rigorous physical and intellectual education is forced to leave his paradise and enter the world, challenging his idea of what it means to be a parent.<br>\
+';
+var d = dataStr.split('<br><br><br>');
+for (var i = 0; i < d.length; i++) {
+    var c = d[i].split('<br><br>');
+    data.push({
+        img: c[0].replace('、', ' ') + '.jpg',
+        caption: c[0].split('、')[1],
+        desc: c[1]
+    });
+    console.log(c[0].replace('、', ' ') + '.jpg');
+};
